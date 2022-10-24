85e59cf75d6ca91dd488e20f0c77a413d4649d98
KaidoMetsa <115225787+KaidoMetsa@users.noreply.github.com>
2022-10-24 20:41:24 +0300

    Push 3

---
+++ Algoritmid/Opilane_fail/JS/Programmid/KIT21E_Pr_01 - Jasper Sistok.py
@@ -0,0 +1,16 @@
+print("Sisesta esimene arv: ")
+arv1 = int(input())
+arv2 = int(input("Sisesta teine arv: "))
+arv3 = arv1 + arv2
+
+print("Summa = ", arv3)
+print("Summa = ", arv1 + arv2)
+
+print(arv1, "+" ,arv2, "=", arv1+arv2)
+print(arv1, "-" ,arv2, "=", arv1-arv2)
+print(arv1, "x" ,arv2, "=", arv1*arv2)
+print(arv1, ":" ,arv2, "=", arv1/arv2)
+print(arv1, "Täisjagamine" ,arv2, "=", arv1//arv2)
+print(arv1, "Jääk" ,arv2, "=", arv1%arv2)
+print(arv1, "Aste" ,arv2, "=", arv1**arv2)
+    
